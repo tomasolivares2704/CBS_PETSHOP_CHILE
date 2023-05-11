bcc037c4c4a4526bd10a5dd2b36bc913f623a45b
Gabriel Bracho <ga.bracho@duocuc.cl>
2023-05-11 18:06:06 -0400

    Actualizo formulario a formato Jquery
    Se realizan cambios en las validaciones pasando a formato Jquery-

--- js/main.js
+++ js/main.js
@@ -1,78 +1,86 @@
-//Función validación formulario
-function validarFormulario(){
-    
-    //Validación de Nombre
-	var nombre = document.getElementById("nombre"); 
-	
-	var validName = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/;
+$(document).ready(function() {
 
-	if( !validName.test(nombre.value) ){
+	//VALIDACIÓN CON JQUERY
+	$('#Formulario').submit(function(event) {
+	  event.preventDefault(); // Evita que el formulario se envíe antes de validarlo correctamente
+
+	  //VARIABLES CORRESPONDIENTES A INPUTS
+
+	  var nombre = $('#nombre').val();
+	  var email = $('#email').val();
+	  var numero = $('#numero').val();
+	  var asunto = $('#asunto').val();
+	  var mensaje = $('#mensaje').val();
+
+	  //VALIDACIONES CAMPOS OBLIGATORIOS
+
+	  if(nombre.length == 0){
+
+		alert("El campo nombre es requerido");
+		return false;
+	}
+
+	  if(email.length == 0){
+
+		alert("El campo correo es requerido");
+		return false;
+	}
+
+	  if(asunto.length == 0){
+
+		alert("El campo asunto es requerido");
+		return false;
+    }
+
+	  if(mensaje.length == 0){
+
+		alert("El campo mensaje es requerido");
+		return false;
+    }
+
+	//LLAMADO DE FUNCIONES DE VALIDACIÓN
+	if(!validarNombre(nombre)){
 
 		alert('El nombre ingresado no es válido, ingrese nuevamente');
 		return false;
 	}
-	  
-	  
-	//Validación de Correo
-	var correo = document.getElementById('email');
-	
-	var validEmail =  /@/;
 
-	if( !validEmail.test(correo.value) ){
+	if(!validarEmail(email)){
 
 		alert('El correo ingresado no es válido, ingrese nuevamente');
 		return false;
 	}
 
-	//Validación Campos Obligatorios
-	var asunto = document.getElementById("asunto");
-	var mensaje = document.getElementById("mensaje");
+	if(!validarNumero(numero)){
 
-	if(!nombre.value){
-
-		alert("El campo nombre es requerido");
-		nombre.focus();
-		verificar==false;
+		alert('El número ingresado no es válido, ingrese nuevamente');
+		return false;
 	}
 
-	if(!correo.value)
-	{
-		alert("El campo correo es requerido");
-		correo.focus();
-		verificar==false;
+	 //MENSAJE CONFIRMACIÓN DE ENVÍO DE FORMULARIO
+	alert('El formulario se ha enviado correctamente');
+    return true;
+
+	});
+
+	//FUNCIONES DE VALIDACIÓN DE REQUISITOS PARA CADA CAMPO
+
+	//NOMBRE
+	function validarNombre(nombre){
+
+		var exprValidName = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/;
+		return exprValidName.test(nombre)
 	}
 
-	if(!asunto.value){
+	function validarEmail(email){
 
-		alert("El campo asunto es requerido");
-		asunto.focus();
-		verificar==false;
-   }
+		var exprValidEmail = /[a-zA-Z0-9]+@[a-zA-Z0-9]+\.(com|net)/;
+		return exprValidEmail.test(email)
+	}
 
-	if(!mensaje.value){
+	function validarNumero(numero){
 
-		alert("El campo mensaje es requerido");
-		mensaje.focus();
-		verificar==false;
-   }
-} 
-
-//Función mensaje número correcto
-function validarNumero(){
-
-	var numero = document.getElementById("numero").value;
-
-	var validNumber = /^(\+?\d{1,3}[\s-]?)?\d{9}$/;
-
-	if( !validNumber.test(numero)){
-
-		alert('El número ingresado no es válido o no está completo');
-		
-	} 
-}
-
-	
-
-
-
-
+		var exprValidNumber = /^(\+?\d{1,3}[\s-]?)?\d{9}$/;
+		return exprValidNumber.test(numero)
+	}
+});
