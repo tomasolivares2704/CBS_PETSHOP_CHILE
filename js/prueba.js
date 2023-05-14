f177d68a1ba3349298de9513d2cbbd610c882a42
Gabriel Bracho <ga.bracho@duocuc.cl>
2023-05-14 14:03:47 -0400

    Mini actualización

--- js/prueba.js
+++ js/prueba.js
@@ -1,3 +1,16 @@
+$.ajax({
+    url: 'https://api.example.com/data',
+    method: 'GET',
+    dataType: 'json',
+    success: function(response) {
+        console.log(response);
+        // Aquí puedes trabajar con la respuesta de la API
+    },
+    error: function(error) {
+        console.log(error);
+        // Aquí puedes manejar los errores de la petición
+    }
+});
 
   
   
